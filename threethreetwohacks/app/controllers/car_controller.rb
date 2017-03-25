7c00c0f107060b868a4e84010c576fe65741ff34
Noam Hacker <nhacker95@gmail.com>
2017-03-25 23:56:59 +0000

    show addresses of cars in index

--- threethreetwohacks/app/controllers/car_controller.rb
+++ threethreetwohacks/app/controllers/car_controller.rb
@@ -1,13 +1,17 @@
 class CarController < ApplicationController
     def index
         @client = Mysql2::Client.new(:host => ENV['IP'], :username => ENV['C9_USER'], :database => "KTCS")
-        @car = @client.query("SELECT * FROM car")
-        @car_array = []
-        @car.each do |car|
-            car_hash = {}
-            car_hash['make'] = car['make']
-            car_hash['model'] = car['model']
-            @car_array.push(car_hash)
-        end
+        @car = @client.query("SELECT make, model, year, daily_fee, address FROM car inner join parking_location where car.parking_locations_pl_id=parking_location.pl_id")
+        # @car_array = []
+        # @car.each do |car|
+        #     car_hash = {}
+        #     car_hash['make'] = car['make']
+        #     car_hash['model'] = car['model']
+        #     car_hash['year'] = car['year']
+        #     car_hash['daily_fee'] = car['daily_fee']
+        #     car_hash['address'] = car['address']
+        #     @car_array.push(car_hash)
+        # end
+        # puts @client.query('select address from car inner join parking_location where car.parking_locations_pl_id=parking_location.pl_id and vin=1000;').first['address']
     end
 end
