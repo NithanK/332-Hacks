8a0c7d20718c9b2b639849c9df96940e5e6899e8
Noam Hacker <nhacker95@gmail.com>
2017-03-25 22:22:27 +0000

    backend logic instead

--- threethreetwohacks/app/controllers/car_controller.rb
+++ threethreetwohacks/app/controllers/car_controller.rb
@@ -2,5 +2,12 @@ class CarController < ApplicationController
     def index
         @client = Mysql2::Client.new(:host => ENV['IP'], :username => ENV['C9_USER'], :database => "KTCS")
         @car = @client.query("SELECT * FROM car")
+        @car_array = []
+        @car.each do |car|
+            car_hash = {}
+            car_hash['make'] = car['make']
+            car_hash['model'] = car['model']
+            @car_array.push(car_hash)
+        end
     end
 end
