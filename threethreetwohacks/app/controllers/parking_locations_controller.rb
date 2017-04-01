03ad103344f8eaac945ad9cb5c1889bf8ba9bb5a
Noam Hacker <nhacker95@gmail.com>
2017-04-01 22:32:39 +0000

    implement add parking location

--- threethreetwohacks/app/controllers/parking_locations_controller.rb
+++ threethreetwohacks/app/controllers/parking_locations_controller.rb
@@ -5,4 +5,12 @@ class ParkingLocationsController < ApplicationController
         @location = client.query("SELECT * FROM parking_location")
        
     end 
+    def create
+        address = params['address']
+        pl_id = params['pl_id']
+        num_spaces = params['num_spaces']
+        client = Mysql2::Client.new(:host => ENV['IP'], :username => ENV['C9_USER'], :database => "KTCS")
+        client.query("insert into parking_location (pl_id, address, num_spaces) values ('#{pl_id}','#{address}','#{num_spaces}')")
+        redirect_to '/parking_locations'
+    end
 end
