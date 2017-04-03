dc20b63673fef1b79d678f6734072fff7546ff90
Noam Hacker <nhacker95@gmail.com>
2017-04-03 17:25:09 +0000

    restrict certain pages to admin only

--- threethreetwohacks/app/controllers/application_controller.rb
+++ threethreetwohacks/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   ENV['TZ'] = 'EST' # set the timezone
-  ENV['ROOT_PATH'] = 'https://threethreetwohacks-tjatjenahej.c9users.io'
+  ENV['ROOT_PATH'] = 'https://threethreetwohacks-noamhacker.c9users.io'
   include SessionsHelper
 end
