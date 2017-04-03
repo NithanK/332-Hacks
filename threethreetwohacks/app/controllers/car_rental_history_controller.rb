308f09c79e23241108a6c5fd3a51c665b0c81498
Noam Hacker <nhacker95@gmail.com>
2017-04-03 19:47:11 +0000

    general improvements

--- threethreetwohacks/app/controllers/car_rental_history_controller.rb
+++ threethreetwohacks/app/controllers/car_rental_history_controller.rb
@@ -5,10 +5,13 @@ class CarRentalHistoryController < ApplicationController
         @client = Mysql2::Client.new(:host => ENV['IP'], :username => ENV['C9_USER'], :database => "KTCS")
         query_string = "SELECT * FROM car INNER JOIN car_rental_history on car.VIN = car_rental_history.car_VIN WHERE VIN ="+vin
         @carHistory =  @client.query(query_string)
+        @car = @carHistory.each[0]
+        @car = "#{@car['make']} #{@car['model']} (#{@car['year']})"
         @carHistory_array = []
         @carHistory.each do |carHistory|
             carHistory_hash = {}
             carHistory_hash['make'] = carHistory['make']
+            carHistory_hash['date'] = carHistory['date']
             carHistory_hash['model'] = carHistory['model']
             carHistory_hash['status_on_return'] = carHistory['status_on_return']
              carHistory_hash['distance'] = carHistory['distance']
